dfe558ea462b970f1ccbc490b040bc104171c99f
Mustapha <mustapha.daghour@universite-paris-saclay.fr>
2023-04-16 20:52:42 +0200

    bb
    bbb

--- The-Cursed-Mansion-v2/Assets/Scripts/Pause.cs
+++ The-Cursed-Mansion-v2/Assets/Scripts/Pause.cs
@@ -1,24 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private GameObject panel;
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenu;
+    private Button btnRejouer;
+    private GameObject player;
+    private Vector3 startPosition;
+
     void Start()
     {
-        panel = GameObject.Find("PanelPause");
-        panel.SetActive(false);
-       
+        btnRejouer = GameObject.Find("RetryButton").GetComponent<Button>();
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPosition = player.transform.position;
     }
 
-   void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("Touche Échap détectée !");
-            panel.SetActive(true); 
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
         }
+    }
+
+    public void Paused()
+    {
+        //PlayerMovement.instance.enabled = false;
+        //activer le menu pause c'est à dire l'afficher
+        pauseMenu.SetActive(true);
+        //arréter le temps
+        Time.timeScale = 0;
+        //changer le statut du jeu
+        gameIsPaused = true;
+
+        // Ajouter la méthode RestartGame à l'événement onClick du bouton RetryButton
+        btnRejouer.onClick.AddListener(RestartGame);
+    }
+
+    public void RestartGame()
+    {
+        // Supprimer la méthode RestartGame de l'événement onClick du bouton RetryButton
+        btnRejouer.onClick.RemoveAllListeners();
+
+        // changer le statut du jeu
+        gameIsPaused = false;
+
+        // réinitialiser la position du joueur
+        player.transform.position = startPosition;
+
+        // réactiver le mouvement du joueur
+        //PlayerMovement.instance.enabled = true;
+
+        // relancer le temps
+        Time.timeScale = 1;
+
+        // désactiver le menu pause
+        pauseMenu.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        //reglage des problème de saut
+        //PlayerMovement.instance.enabled = true;
+        //desactiver le menu pause c'est à dire l'afficher
+        pauseMenu.SetActive(false);
+        //relancer le temps
+        Time.timeScale = 1;
+        //changer le statut du jeu
+        gameIsPaused = false;
     }
 }
