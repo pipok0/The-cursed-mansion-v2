28a3998cc328db3792dc5e0a209b0f3dc04180d0
pipok0 <pipoko914@gmail.com>
2023-04-16 20:45:21 +0200

    Ajout du point faible du fantome

---
+++ The-Cursed-Mansion-v2/Assets/Scripts/WeakSpot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakSpot : MonoBehaviour
+{
+    public GameObject objectToDestroy;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            Destroy(objectToDestroy);
+        }
+    }
+}
