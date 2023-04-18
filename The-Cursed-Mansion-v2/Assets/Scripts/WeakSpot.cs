b1a6d734346f353e71c0d78134883835f53547e1
Barol-b <130918840+Barol-b@users.noreply.github.com>
2023-04-18 18:43:23 +0200

    ajout de points quand ennemi tué
    /

--- The-Cursed-Mansion-v2/Assets/Scripts/WeakSpot.cs
+++ The-Cursed-Mansion-v2/Assets/Scripts/WeakSpot.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WeakSpot : MonoBehaviour
 {
     public GameObject objectToDestroy;
+    public Inventory inventory;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
             Destroy(objectToDestroy);
+            inventory.AddCoins(1);
         }
     }
 }
