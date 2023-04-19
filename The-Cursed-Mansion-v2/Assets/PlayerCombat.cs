2a0748e4400495fc121f8f63c506d2c6ca8b335e
pipok0 <124078386+pipok0@users.noreply.github.com>
2023-04-19 11:35:17 +0200

    Rectification de l'animation d'attaque

--- The-Cursed-Mansion-v2/Assets/PlayerCombat.cs
+++ The-Cursed-Mansion-v2/Assets/PlayerCombat.cs
@@ -23,21 +23,21 @@ public class PlayerCombat : MonoBehaviour
         {  
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                Attack();
+                StartCoroutine(Attack());
                 nextAttackTime = Time.time + 1f / attackRate;
             }
         }
     }
 
-    void Attack()
+    IEnumerator Attack()
     {
-
         // Animation d'attaque
         animator.SetTrigger("Attack");
 
         // Détecte les ennemis à notre portée
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
+        yield return new WaitForSeconds(0.2f);
 
         // Leur infliger des dégâts
         foreach (Collider2D enemy in hitEnemies)
