4ed53fa38be85513b47896d45b9f361258000356
pipok0 <pipoko914@gmail.com>
2023-04-19 19:23:16 +0200

    Ajout des scripts et animation

---
+++ The-Cursed-Mansion-v2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    public GameObject bloodEffect;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Instantiate(bloodEffect, transform.position, Quaternion.identity) ;
+        currentHealth -= damage;
+
+        // Play hurt animation
+
+        if(currentHealth <= 0)
+        {
+            Die();
+            animator.SetTrigger("GhostDeath");
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ The-Cursed-Mansion-v2/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+
+    public Animator animator;
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+    public float attackRate = 1f;
+    float nextAttackTime = 0f;
+    public bool isAttacking = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time >= nextAttackTime)
+        {  
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+
+        // Animation d'attaque
+        animator.SetTrigger("Attack");
+
+        // Détecte les ennemis à notre portée
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+
+        // Leur infliger des dégâts
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
